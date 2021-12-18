039a4ad14ff4e8001af383d0eb76f15685415505
Lucian Kallee <luciankallee@gmail.com>
2021-12-18 18:58:50 +0200

    Update

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	
+</script>
+
+<main class="">
+	<button class="w-28 bg-red-400 hover:bg-red-600 text-white p-2 rounded-full">
+		Click
+	</button>
+</main>
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+  </style>
